086fdc25ceb8c2daa628e55b568b6c96563461a3
breja <gh@breja.io>
2018-12-22 23:50:19 -0500

    cleanup ur room son

--- src/bootstrap/galaxy.ts
+++ src/bootstrap/galaxy.ts
@@ -8,13 +8,14 @@
 import * as instanceManager from '../core/instance-manager'
 import { resize } from '../core/master-control'
 import * as workspace from '../core/workspace'
-// import { requireDir } from '../support/utils'
+import { requireDir } from '../support/utils'
 import '../render/redraw'
 
 // TODO: user-menu and user-overlay-menu not calling user callbacks!
 // TODO: check nvim services instances to make sure they are starting up correctly
 // TODO: hide black canvas while webgl is init
 // TODO: need to show where code actions are available
+// TODO: webgl line width
 
 workspace.on('resize', ({ rows, cols }) => resize(cols, rows))
 workspace.resize()
@@ -32,46 +33,8 @@ requestAnimationFrame(() => {
   // TODO: use requireDir once we can load all components safely
   setTimeout(() => {
     require('../services/remote')
-
-    require('../components/change-project')
-    require('../components/vim-create')
-    require('../components/vim-switch')
-    require('../components/vim-rename')
-    require('../components/buffers')
-    require('../components/files')
-    require('../components/explorer')
-    require('../components/color-picker')
-    require('../components/nc')
-    require('../components/inventory')
-    require('../components/divination')
-    require('../components/debug')
-    require('../components/generic-menu')
-    require('../components/generic-prompt')
-    require('../components/user-menu')
-    require('../components/user-overlay-menu')
-    require('../components/buffer-search')
-    require('../components/viewport-search')
-    require('../components/grep')
-
-    // AI
-    require('../components/autocomplete')
-    require('../components/hint')
-    require('../components/symbols')
-    require('../components/references')
-    require('../components/hover')
-    require('../components/problem-info')
-    require('../components/code-actions')
+    requireDir(`${__dirname}/../components`)
+    //   TODO: load dependency-manager from a worker
+    //   setTimeout(() => require('../support/dependency-manager').default(), 100)
   }, 199)
-
-
-  // requestAnimationFrame(() => {
-  //   // TODO: can we load copmonents on demand?
-  //   // aka, either load when user requests, or after 10 sec of app startup shit
-  //   // in the inventory PR, layer actions are now setup to require the componet.
-  //   // this could be a way to lazy load components (or maybe some of the
-  //   // non-important ones - color-picker, etc.)
-  //   requireDir(`${__dirname}/../components`)
-  //   requestAnimationFrame(() => require('../core/ai'))
-  //   setTimeout(() => require('../support/dependency-manager').default(), 100)
-  // })
 })
