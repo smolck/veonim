e4cecbb5ca26feef03e54ae98af2b8cefbd8469c
breja <gh@breja.io>
2018-12-31 17:03:02 -0500

    vsc.terminal get pid and sendText func

--- src/neovim/function-types.ts
+++ src/neovim/function-types.ts
@@ -45,22 +45,24 @@ interface Position {
 type WindowPosition = [ string, number, number, number ]
 
 export interface Functions {
-  VeonimState(): Promise<State>,
-  VeonimPosition(): Promise<Position>,
-  VeonimCallEvent(event: string): void,
-  VeonimCallback(id: number, result: any): void,
-  Buffers(): Promise<VimBuffer[]>,
-  OpenPaths(): Promise<string[]>,
-  getcwd(): Promise<string>,
-  getline(type: string | number, end?: string): Promise<string | string[]>,
-  expand(type: string): Promise<string>,
-  synIDattr(id: number, type: string): Promise<number>,
-  getpos(where: string): Promise<WindowPosition>,
-  setloclist(window: number, list: QuickFixList[]): Promise<void>,
-  getqflist(): Promise<QuickFixList[]>,
-  cursor(line: number, column: number): Promise<void>,
-  bufname(expr: string | number): Promise<string>,
-  getbufline(expr: string | number, startLine: number, endLine?: number | string): Promise<string[]>,
-  getbufvar(expr: string | number, varname?: string, defaultValue?: any): Promise<any>,
-  termopen(cmd: string, options: object): void,
+  VeonimState(): Promise<State>
+  VeonimPosition(): Promise<Position>
+  VeonimCallEvent(event: string): void
+  VeonimCallback(id: number, result: any): void
+  Buffers(): Promise<VimBuffer[]>
+  OpenPaths(): Promise<string[]>
+  getcwd(): Promise<string>
+  getline(type: string | number, end?: string): Promise<string | string[]>
+  expand(type: string): Promise<string>
+  synIDattr(id: number, type: string): Promise<number>
+  getpos(where: string): Promise<WindowPosition>
+  setloclist(window: number, list: QuickFixList[]): Promise<void>
+  getqflist(): Promise<QuickFixList[]>
+  cursor(line: number, column: number): Promise<void>
+  bufname(expr: string | number): Promise<string>
+  getbufline(expr: string | number, startLine: number, endLine?: number | string): Promise<string[]>
+  getbufvar(expr: string | number, varname?: string, defaultValue?: any): Promise<any>
+  termopen(cmd: string, options: object): void
+  jobpid(jobId: number): Promise<number>
+  chansend(id: number, data: string | string[]): Promise<number>
 }
--- src/vscode/terminal.ts
+++ src/vscode/terminal.ts
@@ -7,17 +7,12 @@ export default (bufid: number): vsc.Terminal => ({
     return nvimSync((nvim, id) => nvim.Buffer(id).name).call(bufid)
   },
   get processId() {
-    // TODO: is this the correct way of getting the pid? i can't find it in the docs
-    // nvim.Buffer(bufid).getVar('b:terminal_job_pid')
-
-      // *b:terminal_job_pid*	PID of the top-level process in a |:terminal|.
-      // Use `jobpid(&channel)` instead.
-
+    return nvim.Buffer(bufid).getVar('channel').then(channelId => nvim.call.jobpid(channelId))
   },
-  sendText: (text, addNewLine = true) => {
-    // TODO: how do we get the channel ID and do chansend? nvim api or have to eval?
-    // - |'channel'| The nvim channel ID for the underlying PTY.
-    //   |chansend()| can be used to send input to the terminal.
+  sendText: async (text, addNewLine = true) => {
+    const channelId = await nvim.Buffer(bufid).getVar('channel')
+    // as per nvim docs, to send a final newline, include a final empty string
+    nvim.call.chansend(channelId, addNewLine ? [ text, '' ] : text)
   },
   show: () => {
     // TODO: not sure about the UX here. we don't use terminal panel like vscode
