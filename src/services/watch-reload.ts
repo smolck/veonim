89cedf99eaf35d70218bacb3b27b4e41efaa580e
breja <gh@breja.io>
2019-01-12 22:18:11 -0500

    only register one filewatcher per file

--- src/services/watch-reload.ts
+++ src/services/watch-reload.ts
@@ -6,6 +6,7 @@ const watchers = new Map<string, any>()
 nvim.on.bufLoad(async () => {
   const filepath = nvim.state.absoluteFilepath
   if (!filepath) return
+  if (watchers.has(filepath)) return
   if (!await exists(filepath)) return
   const w = await watchFile(filepath, () => nvim.cmd(`checktime ${filepath}`))
   watchers.set(filepath, w)
