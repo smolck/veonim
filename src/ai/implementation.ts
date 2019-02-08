57cbec563a0f89449a44212d351d7822b2bd0d90
breja <gh@breja.io>
2019-02-07 21:46:29 -0500

    goto implementation now uses vscode extension

--- src/ai/implementation.ts
+++ src/ai/implementation.ts
@@ -1,36 +1,20 @@
-import { EditorLocation, implementation, definition } from '../langserv/adapter'
-import { supports } from '../langserv/server-features'
+import { vscode } from '../core/extensions-api'
+import { PromiseBoss } from '../support/utils'
 import nvim from '../neovim/api'
 
-interface StateLocation {
-  absoluteFilepath: string
-  line: number
-  column: number
+const boss = PromiseBoss()
+
+const doImplementation = async () => {
+  const result = await boss.schedule(vscode.language.provideImplementation(), { timeout: 3e3 })
+  if (!result) return
+  const { path, range: { start } } = result
+
+  nvim.jumpTo({
+    path,
+    line: start.line,
+    column: start.character,
+  })
 }
 
-const recursiveDefinition = async (stateLocation: StateLocation): Promise<EditorLocation> => {
-  const { path, line, column } = await definition(Object.assign(nvim.state, stateLocation))
-
-  if (!path || !line || !column) return {
-    path: stateLocation.absoluteFilepath,
-    line: stateLocation.line,
-    column: stateLocation.column,
-  }
-
-  return recursiveDefinition({ line, column, absoluteFilepath: path })
-}
-
-nvim.onAction('implementation', async () => {
-  const implementationSupported = supports.implementation(nvim.state.cwd, nvim.state.filetype)
-  const definitionSupported = supports.definition(nvim.state.cwd, nvim.state.filetype)
-  const anySupport = implementationSupported || definitionSupported
-
-  if (!anySupport) return
-
-  const { path, line, column } = implementationSupported
-    ? await implementation(nvim.state)
-    : await recursiveDefinition(nvim.state)
-
-  if (!line || !column) return
-  nvim.jumpTo({ path, line, column })
-})
+nvim.onAction('implementation', doImplementation)
+export default doImplementation
