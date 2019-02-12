d5c25261c10c6616abaa175861d466480d7bb8ed
breja <gh@breja.io>
2019-02-11 22:28:27 -0500

    wip: rename with vscode extension

--- src/ai/rename.ts
+++ src/ai/rename.ts
@@ -1,23 +1,30 @@
-import { rename, textSync } from '../langserv/adapter'
-import { supports } from '../langserv/server-features'
+import { vscode } from '../core/extensions-api'
+import { PromiseBoss } from '../support/utils'
+import { Position } from '../vscode/types'
 import nvim from '../neovim/api'
+
+const boss = PromiseBoss()
 
 // TODO: anyway to improve the glitchiness of undo/apply edit? any way to also pause render in undo
 // or maybe figure out how to diff based on the partial modification
 // call atomic? tricky with getting target lines for replacements
 // even if done before atomic operations, line numbers could be off
 const doRename = async () => {
-  if (!supports.rename(nvim.state.cwd, nvim.state.filetype)) return
-
-  textSync.pause()
-  const editPosition = { line: nvim.state.line, column: nvim.state.column }
+  vscode.textSync.pause()
+  const position = new Position(nvim.state.line, nvim.state.column)
   nvim.feedkeys('ciw')
   await nvim.untilEvent.insertLeave
   const newName = await nvim.expr('@.')
   nvim.feedkeys('u')
-  textSync.resume()
+  vscode.textSync.resume()
 
-  nvim.applyPatches(await rename({ ...nvim.state, ...editPosition, newName }))
+  if (!newName) return
+  const edits = await boss.schedule(vscode.language.provideRenameEdits(newName, position), { timeout: 10e3 })
+  // TODO: why should we apply the patches here? why not have the providers.provideRenameEdits call
+  // workspace.applyEdit directly and have that modify the documents?
+  // make sure we support undos
+  console.warn('NYI: rename edits:', edits)
+  // nvim.applyPatches(await rename({ ...nvim.state, ...editPosition, newName }))
 }
 
 nvim.onAction('rename', doRename)
