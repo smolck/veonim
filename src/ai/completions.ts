632d15d2b46376710f8a7180b67f4fcc6c363304
breja <gh@breja.io>
2019-02-24 20:26:58 -0500

    completions fix insert snippets - not available yet

--- src/ai/completions.ts
+++ src/ai/completions.ts
@@ -110,7 +110,11 @@ const getSemanticCompletions = (line: number, column: number) => EarlyPromise(as
     return done(cache.semanticCompletions.get(`${line}:${column}`)!)
 
   // TODO: different 'triggerKind'(s)? what about 'triggerCharacters'?
-  nvim.untilEvent.cursorMoveInsert.then(completionBoss.cancelCurrentPromise)
+  // TODO: i'm not sure if we are ready for this yet?
+  // if we cache completions on the first char, then we will not want to cancel
+  // the first request for the current start index. we should find a way to cancel
+  // more intelligently based on available/relevant completion cache results
+  // nvim.untilEvent.cursorMoveInsert.then(completionBoss.cancelCurrentPromise)
   const completions = await completionBoss.schedule(vscode.language.provideCompletionItems({ triggerKind: 0 }), { timeout: 2e3 })
   if (!completions) return done([])
 
@@ -118,7 +122,9 @@ const getSemanticCompletions = (line: number, column: number) => EarlyPromise(as
   // TODO: do we need to remap this or can we just pass along the completions object as is?
   const options = completions.map(m => ({
     raw: m,
-    insertText: ((m.insertText || {} as any).value as string || m.insertText as string) || m.label,
+    // TODO: snippets not supported yet because no extended marks. try priority of label first
+    // insertText: ((m.insertText || {} as any).value as string || m.insertText as string) || m.label,
+    insertText: m.label || ((m.insertText || {} as any).value as string || m.insertText as string),
     text: m.label,
     kind: m.kind || CompletionItemKind.Text,
   }))
