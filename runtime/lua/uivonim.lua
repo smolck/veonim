0b8ab664e85467bf77aefb848e223cdddc8d510f
smolck <46855713+smolck@users.noreply.github.com>
2020-09-30 13:31:24 -0500

    Position signature help on correct row (thanks to help from @sunjon)

--- runtime/lua/uivonim.lua
+++ runtime/lua/uivonim.lua
@@ -1,11 +1,11 @@
 local uivonim = {}
 local util = vim.lsp.util
 
--- @glepnir
--- local params = lsp.util.make_position_params()
--- local result = vim.lsp.buf_request(0,requestmethod,params,timeout_ms or 1000)
--- (or buf_request_sync, but only if needed accord. @norcalli, otherwise isn't
--- recommended by him)
+local function get_cursor_pos()
+  local pos = vim.api.nvim_win_get_cursor(0)
+  local win_id = vim.fn.win_getid()
+  return pos[1] - vim.fn.getwininfo(win_id)[1].topline, pos[2]
+end
 
 function uivonim.signature_help(_, method, result)
   if not (result and result.signatures and result.signatures[1]) then
@@ -21,11 +21,8 @@ function uivonim.signature_help(_, method, result)
     return
   end
 
-  local pos = vim.api.nvim_win_get_cursor(0)
-  vim.fn.Uivonim('signature-help', method, result, pos[1] - 1, pos[2])
-  -- util.focusable_preview(method, function()
-  --   return lines, util.try_trim_markdown_code_blocks(lines)
-  -- end)
+  local row, col = get_cursor_pos()
+  vim.fn.Uivonim('signature-help', method, result, row, col)
 end
 
 uivonim.lsp_callbacks = {
